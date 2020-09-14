50e8f6d21cbed09f9d9013c0b2e1359b2d43336c
Kevin <kevin@beets3d.cn>
2020-09-14 09:33:19 +0800

    refactor for pypi.org publishing

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pantilt', 
-    version='1.0.0', 
-    packages=['utils'],
+    version='0.0.1', 
+    packages=find_packages(),
     description='A pantilt control utility',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Billy Wang, Kevin Peng, Shawn Ling',
     url='https://github.com/youyoubilly/pantilt',
     keywords=['pan tilt', 'servo control'],
-    install_requires=['Adafruit_PCA9685'
-                    ],
+    install_requires=['bcam','Adafruit_PCA9685'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
