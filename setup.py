5fc0c1fda12313b2a3ca3ad57a29736750ed466c
Kevin <kevin@beets3d.cn>
2020-10-10 17:27:00 +0800

    Fix __init__.py version issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pantilt', 
-    version='0.0.1', 
+    version='0.0.2', 
     packages=find_packages(),
     description='A pantilt control utility',
     long_description=long_description,
