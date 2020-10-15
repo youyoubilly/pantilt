fe2dde0b64071bf193dcc51284fa6a4121d1110e
Billio <youyoubilly@qq.com>
2020-10-15 15:24:10 +0800

    modify names

--- pantilt/pantilt.py
+++ pantilt/pantilt.py
@@ -4,23 +4,24 @@ import time
 import numpy as np
 
 # from bcamtest.bcam import Camera, frame_dp, bgr8_to_jpeg
-import bcam 
-from .pantilt.servo import Servo
+import bcam
+from bcam.utils import bgr8_to_jpeg
+from .servo import ServoGroup
 import ipywidgets.widgets as widgets
 from ipywidgets import Button, HBox, VBox
 import os
 
-class CamPanTilt():
-    def __init__(self):
-        self.pt = PanTilt(bus=1)
-        self.pt.add_rl_servo(channel=13, max_angle=180, min_angle=0)
-        self.pt.add_ud_servo(channel=12, max_angle=180, min_angle=0)
+class PanTilt():
+    def __init__(self, bus=1):
+        self.sg = ServoGroup(bus=bus)
+        self.sg.add_rl_servo(channel=12, max_angle=180, min_angle=0)
+        self.sg.add_ud_servo(channel=13, max_angle=180, min_angle=0)
         self.width_dp = 320
         self.height_dp = 240
         self.image_widget = widgets.Image(format='jpeg', width=self.width_dp, height=self.height_dp)
         self.snap_count = widgets.IntText(description='count:', layout=widgets.Layout(width='140px'), value=0)
-        self.rl_textbox = widgets.IntText(layout=widgets.Layout(width='148px'), value=self.pt.read()[0], description='rl:')
-        self.ud_textbox = widgets.IntText(layout=widgets.Layout(width='148px'), value=self.pt.read()[1], description='ud:')
+        self.rl_textbox = widgets.IntText(layout=widgets.Layout(width='148px'), value=self.sg.read()[0], description='rl:')
+        self.ud_textbox = widgets.IntText(layout=widgets.Layout(width='148px'), value=self.sg.read()[1], description='ud:')
         self.rl_slider = widgets.FloatSlider(min=-1, max=1, value=0, step=0.02, description='rl')
         self.ud_slider = widgets.FloatSlider(min=-1, max=1, value=0, step=0.02, description='ud')
         self.snap_dir = 'snap'
@@ -28,48 +29,49 @@ class CamPanTilt():
         self.controller = None
         self.press_count = 0
         
-    def cam(self, width_cap=800, height_cap=600, fps_cap=2, flip=2):
-        camera = Camera(width=width_cap, height=height_cap, fps=fps_cap, is_usb=False, flip=flip)
-        self.camera_link = traitlets.dlink((camera, 'value'), (self.image_widget, 'value'), transform=frame_dp)
+    def cam(self, cam_type=bcam.JETSON_CAM, width_cap=800, height_cap=600, fps_cap=2, flip=0):
+        camera = bcam.config(cam_type=cam_type).resolution(width_cap, height_cap).rotate_180().fps(2).build()
+        camera.start()
+        self.camera_link = traitlets.dlink((camera, 'value'), (self.image_widget, 'value'), transform=bgr8_to_jpeg)
         return self.image_widget
 
     def _rl_val(self, val):
-        return int((val * (self.pt.rl.max_angle - self.pt.rl.min_angle) + self.pt.rl.max_angle + self.pt.rl.min_angle) / 2)
+        return int((val * (self.sg.rl.max_angle - self.sg.rl.min_angle) + self.sg.rl.max_angle + self.sg.rl.min_angle) / 2)
     
     def _ud_val(self, val):
-        return int((val * (self.pt.rl.max_angle - self.pt.rl.min_angle) + self.pt.rl.max_angle + self.pt.rl.min_angle) / 2)
+        return int((val * (self.sg.rl.max_angle - self.sg.rl.min_angle) + self.sg.rl.max_angle + self.sg.rl.min_angle) / 2)
 
     def _rl_move1(self, change):
         val = change['new']
-        self.pt.to_angle(rl=self._rl_val(val=val))
+        self.sg.to_angle(rl=self._rl_val(val=val))
 
     def _ud_move1(self, change):
         val = change['new']
-        self.pt.to_angle(ud=self._ud_val(val=val))
+        self.sg.to_angle(ud=self._ud_val(val=val))
         
     def _rl_move2(self, change):
         val = change['new']
-        self.pt.to_angle(rl=val)
+        self.sg.to_angle(rl=val)
         
     def _ud_move2(self, change):
         val = change['new']
-        self.pt.to_angle(ud=val)
+        self.sg.to_angle(ud=val)
         
     def _sider_to_left(self, val):
-        return self.pt.rl.max_angle * val - (val - 1) * (self.pt.rl.max_angle - self.pt.rl.min_angle) / 2
+        return self.sg.rl.max_angle * val - (val - 1) * (self.sg.rl.max_angle - self.sg.rl.min_angle) / 2
     
     def _sider_to_right(self, val):
-        return self.pt.rl.min_angle * val - (val - 1) * (self.pt.rl.max_angle - self.pt.rl.min_angle) / 2
+        return self.sg.rl.min_angle * val - (val - 1) * (self.sg.rl.max_angle - self.sg.rl.min_angle) / 2
     
     def _sider_left_cam(self, change):
         val = change['new']
         if self.controller.buttons[7].value == 0:
-            self.pt.to_angle(rl=self._sider_to_left(val=val))
+            self.sg.to_angle(rl=self._sider_to_left(val=val))
 
     def _sider_right_cam(self, change):
         val = change['new']
         if self.controller.buttons[6].value == 0:
-            self.pt.to_angle(rl=self._sider_to_right(val=val))
+            self.sg.to_angle(rl=self._sider_to_right(val=val))
     
     def pos_box(self):
         self.rl_textbox.observe(self._rl_move2, names=['value'])
@@ -105,26 +107,26 @@ class CamPanTilt():
         com_b = VBox([row4, row5, row6])
         
         #step: '↖', '↑', '↗', '←', 'reset', '→', '↙', '↓', '↘'
-        step_items[0].on_click(lambda x: self.pt.go_up_left())
-        step_items[1].on_click(lambda x: self.pt.go_up())
-        step_items[2].on_click(lambda x: self.pt.go_up_right())
-        step_items[3].on_click(lambda x: self.pt.go_left())
-        step_items[4].on_click(lambda x: self.pt.reset())
-        step_items[5].on_click(lambda x: self.pt.go_right())
-        step_items[6].on_click(lambda x: self.pt.go_down_left())
-        step_items[7].on_click(lambda x: self.pt.go_down() )
-        step_items[8].on_click(lambda x: self.pt.go_down_right())
+        step_items[0].on_click(lambda x: self.sg.go_up_left())
+        step_items[1].on_click(lambda x: self.sg.go_up())
+        step_items[2].on_click(lambda x: self.sg.go_up_right())
+        step_items[3].on_click(lambda x: self.sg.go_left())
+        step_items[4].on_click(lambda x: self.sg.reset())
+        step_items[5].on_click(lambda x: self.sg.go_right())
+        step_items[6].on_click(lambda x: self.sg.go_down_left())
+        step_items[7].on_click(lambda x: self.sg.go_down() )
+        step_items[8].on_click(lambda x: self.sg.go_down_right())
 
         #far: '◤', '▲', '◥', '◄', 'reset', '►', '◣', '▼', '◢'
-        far_items[0].on_click(lambda x: self.pt.upper_left())
-        far_items[1].on_click(lambda x: self.pt.far_up())
-        far_items[2].on_click(lambda x: self.pt.upper_right())
-        far_items[3].on_click(lambda x: self.pt.far_left())
-        far_items[4].on_click(lambda x: self.pt.reset())
-        far_items[5].on_click(lambda x: self.pt.far_right())
-        far_items[6].on_click(lambda x: self.pt.lower_left())
-        far_items[7].on_click(lambda x: self.pt.far_down() )
-        far_items[8].on_click(lambda x: self.pt.lower_right())
+        far_items[0].on_click(lambda x: self.sg.upper_left())
+        far_items[1].on_click(lambda x: self.sg.far_up())
+        far_items[2].on_click(lambda x: self.sg.upper_right())
+        far_items[3].on_click(lambda x: self.sg.far_left())
+        far_items[4].on_click(lambda x: self.sg.reset())
+        far_items[5].on_click(lambda x: self.sg.far_right())
+        far_items[6].on_click(lambda x: self.sg.lower_left())
+        far_items[7].on_click(lambda x: self.sg.far_down() )
+        far_items[8].on_click(lambda x: self.sg.lower_right())
         
         widgets_set1 = HBox([com_a, com_b])
         
@@ -142,8 +144,8 @@ class CamPanTilt():
         self.snap_count.value = len(os.listdir(self.snap_dir))
         
     def _read_pos(self):
-        self.rl_textbox.value = self.pt.read()[0]
-        self.ud_textbox.value = self.pt.read()[1]
+        self.rl_textbox.value = self.sg.read()[0]
+        self.ud_textbox.value = self.sg.read()[1]
         
     def panel_func(self):
         button_layout = widgets.Layout(width='100px', height='80px', align_self='center')
@@ -156,7 +158,7 @@ class CamPanTilt():
         func_items[2].button_style=''
         
         func_items[0].on_click(lambda x: self.save_snap())
-        func_items[1].on_click(lambda x: self.pt.change_reset())
+        func_items[1].on_click(lambda x: self.sg.change_reset())
         func_items[2].on_click(lambda x: self._read_pos())
         
         return HBox([func_items[0], func_items[1], func_items[2]])
@@ -182,8 +184,8 @@ class CamPanTilt():
         self.controller.buttons[2].observe(lambda x: self._press_act(event="go_left")) # Far Left
         self.controller.buttons[1].observe(lambda x: self._press_act(event="go_right")) # Far Right
         
-        self.controller.buttons[9].observe(lambda x: self.pt.reset())
-        self.controller.buttons[8].observe(lambda x: self.pt.change_reset())
+        self.controller.buttons[9].observe(lambda x: self.sg.reset())
+        self.controller.buttons[8].observe(lambda x: self.sg.change_reset())
         self.controller.buttons[17].observe(lambda x: self.save_snap())
         
         self.controller.buttons[6].observe(self._sider_left_cam, names=['value']) # Sliding cam to left
@@ -193,13 +195,13 @@ class CamPanTilt():
         self.press_count += 1
         if self.press_count > 7:
             if event == "go_up":
-                self.pt.go_up()
+                self.sg.go_up()
             elif event == "go_down":
-                self.pt.go_down()
+                self.sg.go_down()
             elif event == "go_left":
-                self.pt.go_left()
+                self.sg.go_left()
             elif event == "go_right":
-                self.pt.go_right()
+                self.sg.go_right()
             self.press_count = 0
 
     def joystick_off(self): # Unlinking js to pantilt movement control
